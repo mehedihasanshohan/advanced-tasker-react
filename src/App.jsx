4227a21b462c1a3b45b9cc0f4163b7fae1aaeab9
mehedi hasan <mhshohan04@gmail.com>
2024-10-16 10:55:12 +0600

    projects setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+        <h2 className='text-red-400 font-bold text-center'>welcome to tasker app</h2>
+      <Navbar></Navbar>
+      <Hero></Hero>
+      <Footer></Footer>
+
+    </>
+  )
+}
+
+export default App
